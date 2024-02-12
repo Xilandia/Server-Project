0fec582e44c741c83960298078bb1c2526e9a28d
Omer Segal <omer_segal@live.com>
2024-02-13 00:23:46 +0200

    Update ClientConnection.java
    Implemented capturing packet body while still being able to print just the header.

--- src/ClientConnection.java
+++ src/ClientConnection.java
@@ -21,6 +21,7 @@ public class ClientConnection implements Runnable{
         String fullClientRequest;
         StringBuilder clientRequest = new StringBuilder();
         String line;
+        int contentLength = 0;
         try (
                 BufferedReader inFromClient =
                         new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
@@ -29,10 +30,18 @@ public class ClientConnection implements Runnable{
             line = inFromClient.readLine();
             while (line != null && !line.isEmpty()) {
                 clientRequest.append(line).append("\n");
+                if (line.startsWith("Content-Length: ")) {
+                    contentLength = Integer.parseInt(line.substring("Content-Length: ".length()));
+                }
                 line = inFromClient.readLine();
             }
             fullClientRequest = clientRequest.toString();
             System.out.println("Request header: \n" + fullClientRequest + "\n");
+            if (contentLength > 0) {
+                char[] body = new char[contentLength];
+                inFromClient.read(body, 0, contentLength);
+                fullClientRequest += new String(body);
+            }
             if (!fullClientRequest.isEmpty()) {
                 request = new HTTPRequest(fullClientRequest);
                 response = new HTTPResponse(request);
